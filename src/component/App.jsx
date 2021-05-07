4ffad4e1263220c2e7affc7abd755372d8b02317
Felipe Ribeiro <eusouocara3000@gmail.com>
2021-05-07 09:46:15 +0100

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((singleNote) => (
+        <Note
+          key={singleNote.id}
+          title={singleNote.title}
+          content={singleNote.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
